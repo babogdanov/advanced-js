4a51a4d08b5134b6a723afbbf05e946a142efcd7
Boyan Bogdanov <boyan.bogdanov@accedia.com>
2022-11-30 18:14:23 +0200

    prettier

--- labs/8/index.js
+++ labs/8/index.js
@@ -11,7 +11,7 @@ const events = new Map()
 
 app.post("/event", (req, res) => {
   const { body } = req
-  events.set(currEventId++, {...body, guests: new Map()})
+  events.set(currEventId++, { ...body, guests: new Map() })
   res.send(body)
 })
 
@@ -29,34 +29,34 @@ app.delete("/event/:id", (req, res) => {
   res.send(events.delete(+id))
 })
 
-app.post("/event/:id/booking", (req,res) => {
-    const { body } = req
-    const { id } = req.params
-    const event = events.get(+id)
-    
-    if(event.capacity <= 0) {
-        res.status = 401;
-        res.send('Capacity exceeded for event.')
-    }
+app.post("/event/:id/booking", (req, res) => {
+  const { body } = req
+  const { id } = req.params
+  const event = events.get(+id)
 
-    event.guests.set(currGuestId++,body)
-    console.log(event,event.guests)
-    res.send(`Remaining capacity: ${--event.capacity}`)
+  if (event.capacity <= 0) {
+    res.status = 401
+    res.send("Capacity exceeded for event.")
+  }
+
+  event.guests.set(currGuestId++, body)
+  console.log(event, event.guests)
+  res.send(`Remaining capacity: ${--event.capacity}`)
 })
 
-app.get("/event/:id/booking", (req,res) => {
-    const { id } = req.params
-    res.send([...events.get(+id).guests])
+app.get("/event/:id/booking", (req, res) => {
+  const { id } = req.params
+  res.send([...events.get(+id).guests])
 })
 
 app.get("/event/:id/booking/:bookingId", (req, res) => {
-    const { id, bookingId } = req.params
-    res.send(events.get(+id).guests.get(+bookingId))
+  const { id, bookingId } = req.params
+  res.send(events.get(+id).guests.get(+bookingId))
 })
 
-app.delete("/event/:id/booking/:bookingId", (req,res) => {
-    const { id, bookingId } = req.params
-    res.send(events.get(+id).guests.delete(+bookingId))
+app.delete("/event/:id/booking/:bookingId", (req, res) => {
+  const { id, bookingId } = req.params
+  res.send(events.get(+id).guests.delete(+bookingId))
 })
 
 app.listen(port, () => console.log(`server up on port ${port}`))
